51e840f7f7c0d6a544b24552cd5c07c7a9cce7cf
Flyte27 <apayne27@gmail.com>
2014-11-08 19:24:20 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  subject { page }
+
   describe "Home page" do
     it "should have the right title" do
-    	visit '/static_pages/home'
-    	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    	visit root_path
+    	it{should have_selector('title',
+    	 :text => "Ruby on Rails Tutorial Sample App")}
     end
+  end
+  describe "Contact page" do
+    before { visit contact_path }
+
+ 		it {should have_selector('h1', text: 'Contact')}
+	  it {should have_selector('title', 
+			  text: full_title(''))}
+
+  end
+end
+describe "User pages" do
+  subject {page}
+  describe "signup page" do
+    before {visit _signup_path}
+    it { should have_selector('h1', text: 'Sign Up')}
+    it { should have_selector('title', text: full_title('Sign up'))}
   end
 end
