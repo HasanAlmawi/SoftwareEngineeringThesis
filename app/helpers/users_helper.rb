48741ce2a9bcdc52888f571e443259a3e35ed64b
Flyte27 <apayne27@gmail.com>
2014-11-10 02:34:29 -0500

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
+require 'identicon'
 module UsersHelper
+	#Returns the Gravatar (http://gravatar.com/) for the given user
+	def identicon_for(user)
+		####This is the code for gravatars
+		#gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		#gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png"
+		#image_tag(gravatar_url, alt: user.name, class: "gravatar")
+		
+		#This is the code for identicons
+		identicon_url = Identicon.data_url_for user.email, 50, [255,255,255]
+		image_tag(identicon_url)
+	end		
 end
