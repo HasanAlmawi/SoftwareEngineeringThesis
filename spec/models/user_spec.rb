c709a5a7b8855020c3fb03dae5c4d9bcdf52cdfe
Flyte27 <apayne27@gmail.com>
2014-11-20 20:45:15 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,9 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "example@email.com", password: "foobar", password_confirmation: "foobar") }
+  before do
+    @user = User.new(name: "Example User", email: "example@email.com", password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -21,7 +23,10 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+
+
 
   it { should be_valid }
 
@@ -100,5 +105,10 @@ describe User do
   describe "password too short" do
   	before { @user.password = @user.password_confirmation = "a" * 5 }
   	it{should be_invalid}
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,6 +39,15 @@ describe "User Pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+
+			describe "followed by signout" do
+				before { click_link "Sign out" }
+				it { should have_link('Sign in') }
+			end
+		end
+
+		describe "after saving the user" do
+			it { should have_link('Sign out') }
 		end
 	end
 end
