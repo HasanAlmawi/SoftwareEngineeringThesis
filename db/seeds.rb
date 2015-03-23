cc0d05335c47fa948117c38434ae512417642d1c
Flyte27 <apayne27@gmail.com>
2015-03-23 16:41:57 -0400

    Fixing heroku, great job team...

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-if Rails.env.development?
-	admin = User.create!(name: "Alex Payne", email: "apayne27@gmail.com", password: "foobar", password_confirmation: "foobar")
-	admin.toggle!(:admin)
-	99.times do |n|
-		name = Faker::Name.name
-		email = "example-#{n+1}@random.org"
-		password = "password"
-		User.create!(name: name, email: email, password: password, password_confirmation: password)
-	end
-	Event.create!(title: "Social Guidelines", lecturer: "Fai Kenayem", code: "ABC", description: "A guide to making friends and influencing people.", start_time: "10/3/2015 12:00:00 AM", end_time: "10/3/2015 12:00:00 AM")
-	#Event.create!(title: "Guide to Getting Facebook Likes", lecturer: "Hasan Almawi", code: "BSB")
+admin = User.create!(name: "Alex Payne", email: "apayne27@gmail.com", password: "foobar", password_confirmation: "foobar")
+admin.toggle!(:admin)
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n+1}@random.org"
+	password = "password"
+	User.create!(name: name, email: email, password: password, password_confirmation: password)
 end
+Event.create!(title: "Social Guidelines", lecturer: "Fai Kenayem", code: "ABC", description: "A guide to making friends and influencing people.", start_time: "10/3/2015 12:00:00 AM", end_time: "10/3/2015 12:00:00 AM")
+#Event.create!(title: "Guide to Getting Facebook Likes", lecturer: "Hasan Almawi", code: "BSB")
+
