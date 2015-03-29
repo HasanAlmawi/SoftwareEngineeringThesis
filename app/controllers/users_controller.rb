e271447465be1b994cc770e397d46259f914e4c1
Flyte27 <apayne27@gmail.com>
2015-03-29 18:14:23 -0400

    Changing user redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   	if @user.save
       sign_in @user
       flash[:success] = "Welcome to the attendance system."
-  		redirect_to @user
+  		redirect_to root_path
   	else
   		render 'new'
   	end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
-      redirect_to @user
+      redirect_to root_path
     else
       render 'edit'
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-if Rails.env.development?
-	admin = User.create!(name: "Alex Payne", email: "apayne27@gmail.com", password: "foobar", password_confirmation: "foobar")
-	admin.toggle!(:admin)
-	99.times do |n|
-		name = Faker::Name.name
-		email = "example-#{n+1}@random.org"
-		password = "password"
-		User.create!(name: name, email: email, password: password, password_confirmation: password)
-	end
-	Event.create!(title: "Social Guidelines", lecturer: "Fai Kenayem", code: "ABC", description: "A guide to making friends and influencing people.", start_time: "10/3/2015 12:00:00 AM", end_time: "10/3/2015 12:00:00 AM")
-	Event.create!(title: "Guide to getting Facebook likes", lecturer: "Hasan Almawi", code: "FBL", description: "How to be the coolest person amongst all you friends.", start_time: "26/3/2015 1:30:00 PM", end_time: "26/3/2015 2:30:00 PM")
-	Event.create!(title: "Ruby on Rails coding session", lecturer: "Alex Payne", code: "ROR", description: "A guide to coding in Ruby on Rails.", start_time: "21/3/2015 9:00:00 PM", end_time: "23/3/2015 9:00:00 PM")
+admin = User.create!(name: "Alex Payne", email: "apayne27@gmail.com", password: "foobar", password_confirmation: "foobar")
+admin.toggle!(:admin)
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n+1}@random.org"
+	password = "password"
+	User.create!(name: name, email: email, password: password, password_confirmation: password)
 end
-
-if Rails.env.production?
-	admin = User.create!(name: "Alex Payne", email: "apayne27@gmail.com", password: "foobar", password_confirmation: "foobar")
-	admin.toggle!(:admin)
-	99.times do |n|
-		name = Faker::Name.name
-		email = "example-#{n+1}@random.org"
-		password = "password"
-		User.create!(name: name, email: email, password: password, password_confirmation: password)
-	end
-	Event.create!(title: "Social Guidelines", lecturer: "Fai Kenayem", code: "ABC", description: "A guide to making friends and influencing people.", start_time: "10/3/2015 12:00:00 AM", end_time: "10/3/2015 12:00:00 AM")
-	Event.create!(title: "Guide to getting Facebook likes", lecturer: "Hasan Almawi", code: "FBL", description: "How to be the coolest person amongst all you friends.", start_time: "26/3/2015 1:30:00 PM", end_time: "26/3/2015 2:30:00 PM")
-	Event.create!(title: "Ruby on Rails coding session", lecturer: "Alex Payne", code: "ROR", description: "A guide to coding in Ruby on Rails.", start_time: "21/3/2015 9:00:00 PM", end_time: "23/3/2015 9:00:00 PM")
-end
+Event.create!(title: "Social Guidelines", lecturer: "Fai Kenayem", code: "abi3", faculty: "Dental Science", description: "A guide to making friends and influencing people.", start_time: "10/3/2015 12:00:00 AM", end_time: "10/3/2015 12:00:00 AM")
+Event.create!(title: "Guide to getting Facebook likes", lecturer: "Hasan Almawi", faculty: "Medical Science", code: "fb4l", description: "How to be the coolest person amongst all you friends.", start_time: "26/3/2015 1:30:00 PM", end_time: "26/3/2015 2:30:00 PM")
+Event.create!(title: "Ruby on Rails coding session", lecturer: "Alex Payne", faculty: "Computer Science", code: "r00r", description: "A guide to coding in Ruby on Rails.", start_time: "21/3/2015 9:00:00 PM", end_time: "23/3/2015 9:00:00 PM")
