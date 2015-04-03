5668cf4afd5cfc4211ae9f51f2b67160fd16c32e
Flyte27 <apayne27@gmail.com>
2015-04-03 16:54:52 -0400

    changing a few faculty->department titles

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,7 +7,7 @@ class Attendance < ActiveRecord::Base
 
   def self.to_csv (options = {})
   	CSV.generate(options) do |csv|
-  		csv << ["Email", "Event Title", "Description", "Lecturer", "Faculty", "Date", "Hours"]
+  		csv << ["Email", "Event Title", "Description", "Lecturer", "Department", "Date", "Hours"]
   		all.each do |attendance|
         csv << [User.find(attendance.user_id).email, Event.find_by_code(attendance.code).title, Event.find_by_code(attendance.code).description, 
           Event.find_by_code(attendance.code).lecturer, Event.find_by_code(attendance.code).faculty, Event.find_by_code(attendance.code).start_time.to_date,
